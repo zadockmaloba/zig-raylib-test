82e56e73372f207ef92f86098ba6f94fe02ded16
Zadock Maloba <55388270+zadockmaloba@users.noreply.github.com>
2024-08-21 22:13:52 +0300

    Improve matrix norm function

--- src/common/utils.zig
+++ src/common/utils.zig
@@ -22,3 +22,33 @@ pub fn vec3_normalize(vec: Vector3) Vector3 {
         .z = vec.z * (1 / magnitude),
     };
 }
+
+pub fn normalizePoints(points: []Vector3) void {
+    var max_component: f32 = 0.0;
+    var min_component: f32 = 0.0;
+
+    // Find the maximum component in the matrix
+    for (points) |point| {
+        max_component = if (max_component > point.x) max_component else point.x;
+        max_component = if (max_component > point.y) max_component else point.y;
+        max_component = if (max_component > point.z) max_component else point.z;
+
+        min_component = if (min_component < point.x) min_component else point.x;
+        min_component = if (min_component < point.y) min_component else point.y;
+        min_component = if (min_component < point.z) min_component else point.z;
+    }
+
+    std.debug.print("Max component: {} \n", .{max_component});
+    std.debug.print("Min component: {} \n", .{min_component});
+    const range = max_component - min_component;
+    if (range == 0) return; // Avoid division by zero
+
+    // Normalize each vector by the maximum component
+    for (points) |*point| {
+        point.x = (2.0 * (point.x - min_component) / range) - 1;
+        point.y = (2.0 * (point.y - min_component) / range) - 1;
+        point.z = (2.0 * (point.z - min_component) / range) - 1;
+    }
+
+    std.debug.print("NEW POINTS: {any} \n", .{points});
+}
